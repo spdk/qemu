8dbb7211b264ea375a1ba9377f256933f0fbb7ea
Konrad Sztyber <konrad.sztyber@intel.com>
2019-09-20 12:20:31 +0200

    block/nvme: corrected chunk's slba value
    This patch fixes the value of slba as returned in the chunk information entry.
    It needs to be calculated based on the geometry of the emulated drive.
    
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Change-Id: Ia5809e0414e325f5d0e88bb14f43cd377c5974e9

--- hw/block/nvme_lnvm_helpers.h
+++ hw/block/nvme_lnvm_helpers.h
@@ -273,6 +273,7 @@ static int lnvm_meta_state_set(NvmeNamespace *ns, uint64_t ppa,
     return 0;
 }
 
+
 static inline uint64_t lnvm_chunk_no_to_lba(LnvmCtrl *ln, uint64_t chunk_no)
 {
     uint64_t blk = chunk_no % ln->params.blks_per_pln;
@@ -1187,7 +1188,7 @@ static int lnvm_init_chunk_state(NvmeCtrl *n)
             }
         }
 
-        ppa = i << ln->ppaf.blk_offset;
+        ppa = lnvm_chunk_no_to_lba(ln, i);
         if (written == sec_per_chunk) {
             ln->chunk_state[i].state = LNVM_CHNK_FULL;
         } else if (written != 0) {
